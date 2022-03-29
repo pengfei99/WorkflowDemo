a794563ed83333e0f607fc4aa0fc0a488d49619d
pengfei <liu.pengfei@hotmail.fr>
2022-03-29 12:10:02 +0200

    update generate data profiler

--- src/data_profiling/generate_data_profile.py
+++ src/data_profiling/generate_data_profile.py
@@ -6,15 +6,20 @@ from pandas_profiling import ProfileReport
 from src.log.LogManager import LogManager
 from src.utile import get_local_input_file_path
 
-my_logger = LogManager(__name__).get_logger()
+my_logger = LogManager(__name__,enable_file_handler=True, log_file_path=os.getenv("LOG_PATH")).get_logger()
 my_logger.debug("Init ingest_source_data")
 
 
 def main():
-    remote_file_path = sys.argv[1]
-    data_folder = sys.argv[2]
+    if len(sys.argv) != 3:
+        my_logger.error(
+            'Number of arguments that you give is wrong, please enter the path of the file which you want to analyze.')
+        sys.exit(1)
+    else:
+        remote_file_path = sys.argv[1]
+        data_folder = sys.argv[2]
     input_file_path, file_name = get_local_input_file_path(remote_file_path, data_folder)
-    df = pd.read_csv(input_file_path, sep=",", na_values=[""],index_col=0)
+    df = pd.read_csv(input_file_path, sep=",", na_values=[""], index_col=0)
     my_logger.info("data sample :\n" + str(df.head(5)))
     profile = ProfileReport(df, title="pokemon_data_profiling", minimal=False, explorative=False)
     report_full_path = f"{data_folder}/{file_name}_profile.html"
