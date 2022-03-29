c6a98d47fc36fe6362dc1a5491fea8c05c63670e
pengfei <liu.pengfei@hotmail.fr>
2022-03-29 12:07:53 +0200

    add check duplication

--- command/call_check_duplication.sh
+++ command/call_check_duplication.sh
@@ -1,3 +1,3 @@
-
+export LOG_PATH=/tmp/pokemon
 export PYTHONPATH="${PYTHONPATH}:/home/pliu/git/WorkflowDemo"
-python ../src/data_quality/check_duplication.py s3://pengfei/mlflow-demo/pokemon-cleaned.csv /tmp/pokemon
+python ../src/data_quality/check_duplication.py s3://pengfei/mlflow-demo/pokemon-raw.csv /tmp/pokemon
--- src/data_quality/check_duplication.py
+++ src/data_quality/check_duplication.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
+import os
 import sys
 from string import Template
 
 import pandas as pd
 
+from src.log.LogManager import LogManager
 from src.utile import get_local_input_file_path
+
+my_logger = LogManager(__file__, enable_file_handler=True, log_file_path=os.getenv("LOG_PATH")).get_logger()
+my_logger.debug(f"Init {__file__}")
 
 
 def get_duplicated_rows(data_frame):
@@ -19,13 +24,12 @@ def get_duplicated_row_numbers(duplicate_row_df):
 
 def main():
     if len(sys.argv) != 3:
-        print(
+        my_logger.error(
             'Number of arguments that you give is wrong, please enter the path of the file which you want to analyze.')
     else:
         remote_file_path = sys.argv[1]
         data_folder = sys.argv[2]
         input_file_path, file_name = get_local_input_file_path(remote_file_path, data_folder)
-        output_path = "/tmp/pokemon/duplicate_report.txt"
         df = pd.read_csv(input_file_path, index_col=0)
         result_str = '###################################################\n' \
                      'The total number of duplicated rows is $duplicated_row_number\n' \
@@ -38,13 +42,7 @@ def main():
         duplicated_row_numbers = get_duplicated_row_numbers(duplicated_rows)
         result = temp_obj.substitute(duplicated_row_number=duplicated_row_numbers,
                                      duplicated_rows=duplicated_rows.to_string())
-        print(result)
-        f = open(output_path, "w")
-        if duplicated_row_numbers >= 1:
-            f.write("True")
-        else:
-            f.write("False")
-        f.close()
+        my_logger.info("\n"+result)
 
 
 if __name__ == "__main__":
