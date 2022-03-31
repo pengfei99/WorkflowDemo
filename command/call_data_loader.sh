285812090b57ff40b69883ea9e4ab48d0456c10f
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-03-31 20:02:07 +0200

    update commandd

--- command/call_data_loader.sh
+++ command/call_data_loader.sh
@@ -2,4 +2,4 @@
 export LOG_PATH=/tmp/pokemon
 
 export PYTHONPATH="${PYTHONPATH}:/home/pliu/git/WorkflowDemo"
-python ../src/data_ingestion/s3_data_loader.py /tmp/pokemon pokemon-cleaned.parquet s3://pengfei/workflow-demo/pokemon/target
+python ../src/data_ingestion/s3_data_loader.py /tmp/pokemon pokemon-cleaned.parquet s3a://pengfei/workflow_demo/pokemon/target
--- command/call_data_profile.sh
+++ command/call_data_profile.sh
@@ -1,3 +1,3 @@
 export LOG_PATH=/tmp/pokemon
 export PYTHONPATH="${PYTHONPATH}:/home/pliu/git/WorkflowDemo"
-python ../src/data_profiling/generate_data_profile.py /tmp/pokemon pokemon-cleaned.csv
+python ../src/data_profiling/generate_data_profile.py /tmp/pokemon pokemon-cleaned.csv pokemon-profile-report.html
