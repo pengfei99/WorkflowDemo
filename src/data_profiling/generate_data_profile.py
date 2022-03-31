9dff246f71b5cedcd51951458f828ab83f2c481c
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-03-31 20:02:44 +0200

    change generate report function signature

--- src/data_profiling/generate_data_profile.py
+++ src/data_profiling/generate_data_profile.py
@@ -11,19 +11,24 @@ my_logger.debug("Init ingest_source_data")
 
 
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         my_logger.error(
             'Number of arguments that you give is wrong, please enter the path of the file which you want to analyze.')
         sys.exit(1)
     else:
         data_folder = sys.argv[1]
         file_name = sys.argv[2]
+        report_name = sys.argv[3]
     input_file_path = f"{data_folder}/{file_name}"
     df = pd.read_csv(input_file_path, sep=",", na_values=[""], index_col=0)
     my_logger.info("data sample :\n" + str(df.head(5)))
     profile = ProfileReport(df, title="pokemon_data_profiling", minimal=False, explorative=False)
-    report_full_path = f"{data_folder}/{file_name}_profile.html"
-    profile.to_file(report_full_path)
+    report_full_path = f"{data_folder}/{report_name}"
+    if profile.to_file(report_full_path):
+        my_logger.info(f"Generate report {report_name} for data {file_name} in {data_folder}")
+    else:
+        my_logger.error(f"Fail to generate report for data {file_name}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
